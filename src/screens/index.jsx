4293114f3fae64fd02a49a756a58f6f679fe375c
Pawli <paulatgnz@gmail.com>
2024-10-11 19:48:04 -0300

    change font home

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="w-auto text-4xl rounded-lg drop-shadow-[0_0_24px_rgba(0,0,0,0.5)] h-auto italic text-center mt-36 text-white">
-      <h1 className="text-4xl font-bold mb-4">Bienvenidos a nuestra comunidad</h1>
+      <h1 className="text-3xl italic font-bold mb-4">Bienvenidos a nuestra comunidad</h1>
       <img
         src="/logoB.png"
         className="mx-auto mt-14 h-56 w-auto cursor-pointer hover:rotate-12 transition-transform duration-300"
