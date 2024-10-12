a44ae8bbc2aef6e862d47a01a22e72cf11b269b2
JaviCeRodriguez <javicerodriguez@gmail.com>
2024-10-11 23:33:54 -0300

    fix: footer spaces

--- src/Componentes/Footer/index.jsx
+++ src/Componentes/Footer/index.jsx
@@ -1,7 +1,9 @@
 function Footer() {
   return (
     <footer className="flex justify-center w-full px-2 py-4 text-center rounded-lg drop-shadow-[0_0_100px_rgba(0,0,0,0.7)] text-white">
-      Creado con mucho <p className="hover:scale-150 transition-transform duration-300">❤️</p> por la comunidad
+      Creado con mucho&nbsp;
+      <p className="transition-transform duration-300 hover:scale-150">❤️</p>
+      &nbsp;por la comunidad
     </footer>
   );
 }
