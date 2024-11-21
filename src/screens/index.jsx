b57ddcfab7a56698cd8dbccf07e3d5bfee9bfa50
Pawli <paulatgnz@gmail.com>
2024-11-21 10:30:24 -0300

    add responsive index

--- src/screens/index.jsx
+++ src/screens/index.jsx
@@ -1,10 +1,10 @@
 const Home = () => {
   return (
-    <div className="w-auto text-4xl rounded-lg drop-shadow-[0_0_24px_rgba(0,0,0,0.5)] h-auto italic text-center mt-36 text-white">
-      <h1 className="sm:text-7xl italic font-bold mb-4 text-3xl">Bienvenidos</h1>
+    <div className="flex flex-col items-center justify-center h-screen max-w-screen-xl mx-auto text-white">
+      <h1 className="text-3xl sm:text-7xl italic font-bold mb-6 text-center">Bienvenidos</h1>
       <img
         src="/logoB.png"
-        className="mx-auto mt-14 h-56 w-auto cursor-pointer hover:rotate-12 transition-transform duration-300"
+        className="h-56 w-auto cursor-pointer hover:rotate-12 transition-transform duration-300"
         alt="Logo"
       />
     </div>
